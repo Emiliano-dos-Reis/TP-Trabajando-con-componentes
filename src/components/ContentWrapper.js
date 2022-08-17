c1904aa55cab3b1e8c5c048c086ad0d9f91dfd57
Emiliano-dos-Reis <edosreis26@gmail.com>
2022-08-17 18:28:31 -0300

    Trabajando con componentes

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -1,45 +1,22 @@
 import React from 'react';
-import ContentRowMovies from './ContentRowMovies';
+import TopBar from './TopBar';
+import ContentRowTop from './ContentRowTop';
 import Footer from './Footer';
-import Genresindb from './GenresInDb';
-import Lastmovieindb from './LastMovieInDb';
-import Topbar from './TopBar';
-
-const Contentwrapper = () => {
+import Table from './Table';
+function ContentWrapper(){
     return (
-        <div id="content-wrapper" className="d-flex flex-column">
-
-            <div id="content">
-
-                <Topbar />
-
-                <div className="container-fluid">
-                    <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
-                    </div>
-
-                    <ContentRowMovies />
-
-
-                    <div className="row">
-
-
-                        <Lastmovieindb />
-
-                        <Genresindb />
-
-                    </div>
+        <React.Fragment>
+            {/*<!-- Content Wrapper -->*/}
+            <div id="content-wrapper" className="d-flex flex-column">
+                {/*<!-- Main Content -->*/}
+                <div id="content">
+                    <TopBar />
+                    <ContentRowTop />
+                    <Table/>
+                    <Footer />
                 </div>
-
-            </div>
-
-
-
-            <Footer />
-
-
-        </div>
-    );
+            </div>    
+        </React.Fragment>
+    )
 }
-
-export default Contentwrapper;
+export default ContentWrapper;
